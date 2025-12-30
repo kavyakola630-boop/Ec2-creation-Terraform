68b058de6ce10c568fe7c78afec110aa04cfb2b4
kavyakola630-boop <kavyakola@gmail.com>
2025-12-30 22:01:58 +0530

    added-ec2

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc_security_group_ingress_rule" "myingressrules" {
 } 
 resource "aws_key_pair" "myownkey" {
   key_name = "mysshownkey" 
-  public_key = file("~/.ssh/id_ed25519.pub")
+  public_key = file(pathexpand("~/.ssh/id_ed25519.pub"))
 } 
 resource "aws_instance" "myec2" {
   ami = "ami-02b8269d5e85954ef" 
