0d15bcbefd9494b075169dcd326891f47cbd2bb9
kavyakola630-boop <kavyakola@gmail.com>
2025-12-31 21:50:12 +0530

    added-ec2

--- main.tf
+++ main.tf
@@ -13,17 +13,19 @@ resource "aws_vpc_security_group_ingress_rule" "myingressrules" {
   ip_protocol = "tcp" 
   cidr_ipv4 = "0.0.0.0/0"
 } 
-resource "tls_private_key" "ec2_key" {
-  algorithm = "ED25519"
+data "aws_ami" "myapsouthami" {
+  filter {
+    name = "name" 
+    values = [ "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20250925" ]
+  } 
+  owners = [ "099720109477" ]
 }
-
 resource "aws_key_pair" "myownkey" {
-  key_name   = "mysshownkey"
-  public_key = tls_private_key.ec2_key.public_key_openssh
-}
-
+  key_name = "mysshownkey" 
+  public_key = file("~/id_rsa.pub") 
+} 
 resource "aws_instance" "myec2" {
-  ami = "ami-02b8269d5e85954ef" 
+  ami = data.aws_ami.myapsouthami.id 
   instance_type = "t3.micro" 
   key_name = aws_key_pair.myownkey.key_name 
   associate_public_ip_address = true 
--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+        source = "hashicorp/aws" 
+        version = "~>6.27.0"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-  }
+  } 
+  required_version = "~>1.14.1"
+} 
+provider "aws" { 
+  region = "ap-south-1"
 }
